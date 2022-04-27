cb331246ce15051d8a318822324566f1f6c07dae
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-27 14:20:29 +0530

    Add project files.

---
+++ FindDuplicateString/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindDuplicateString
+{
+    internal class Class1
+    {
+        public static void Duplicate()
+        {
+
+            string data = "Noida short for New Okhla Industrial Development Authority, is a planned city located in Gautam Buddha Nagar district of the Indian state of Uttar Pradesh.";
+
+            int count;
+            char[] charArr = data.ToCharArray();
+
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                count = 1;
+                for (int j = i + 1; j < charArr.Length; j++)
+                {
+                    if (charArr[i] == charArr[j] && charArr[i] != ' ')
+                    {
+                        count++;
+                        charArr[j] = '0';
+                    }
+                }
+                if (count > 1 && charArr[i] != '0')
+                {
+                    Console.WriteLine(charArr[i]);
+                }
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FindDuplicateString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindDuplicateString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.Duplicate();
+        }
+    }
+}
